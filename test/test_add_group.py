8d046906a20d468346b182f3086880dbca6ee15d
Olga <olga.beletskaya@gmail.com>
2015-08-16 11:31:42 +1000

    Class sessionHelper extracted
    Выделен вспомогательный класс для управления сессиями sessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,14 +14,14 @@ def app(request):
 
     
 def test_add_group(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="first", header="dfadsf", footer="dfdsf"))
-        app.logout()
+        app.session.logout()
 
 def test_add_empty_group(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+        app.session.logout()
 
 
 
