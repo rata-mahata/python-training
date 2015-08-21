1f07adacd8683c12dade422f0604123986ef0586
Olga <olga.beletskaya@gmail.com>
2015-08-21 22:33:58 +1000

    added test modify_group with if-else condition

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,12 @@ __author__ = 'Olga'
 
 from model.group import Group
 
-def test_modify_first_group(app):
+def test_modify_group_name(app):
         app.session.login(username="admin", password="secret")
-        app.group.modify_first_group(Group(name="five", header="six", footer="seven"))
+        app.group.modify_first_group(Group(name="new group"))
+        app.session.logout()
+
+def test_modify_group_header(app):
+        app.session.login(username="admin", password="secret")
+        app.group.modify_first_group(Group(header="new header"))
         app.session.logout()
